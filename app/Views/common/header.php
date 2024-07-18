0b0ff8226a719d4985388addf439bd546d627e56
simunde <sidmundece17@gmail.com>
2024-07-18 17:13:21 -0400

    added logo

--- app/Views/common/header.php
+++ app/Views/common/header.php
@@ -6,9 +6,14 @@
     <link rel="stylesheet" type="text/css" href="/styles.css">
 </head>
 <body>
+<header>
+        <div class="header-container">
+          <a href="/"><img src="/images/logo.png" alt="Logo" class="logo"></a>
     <nav>
         <a href="/vendor.php">Vendor Application</a> | 
         <a href="/organizer.php">Organizer Dashboard</a> | 
         <a href="/view_applications.php">View Applications</a> | 
         <a href="/track_status.php">Track Application Status</a>
     </nav>
+    </div>
+    </header>
